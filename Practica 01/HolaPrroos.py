802c045882016f4a9eba89442e981443a17abd04
ElAbriii <111930723+ElAbriii@users.noreply.github.com>
2022-08-23 19:33:35 -0500

    Pruebas 01

---
+++ Practica 01/HolaPrroos.py
@@ -0,0 +1,11 @@
+# Ejemplo saludo
+# Comentarios.- Los comentarios sirven para documentar y detallas cada paso del código o programa
+# Los comentarios los omite o no los toma en cuenta el lenguaje de programación Phyton
+
+# IMRPIMIR O MOSTRAR EN PANTALLA LOS RESULTADOS
+
+print("Hola Prros, a ver, cuánto es 8*5?")
+print("El prro:")
+print(8*5)
+print("Felicidaes Prrossss")
+print()
